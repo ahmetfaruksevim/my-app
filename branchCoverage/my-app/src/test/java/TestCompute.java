32ee94d6398565ed93e2ef807c062fd601727a38
ahmetfaruksevim <125396823+ahmetfaruksevim@users.noreply.github.com>
2024-03-31 22:52:38 +0300

    Test files have been updated.
    Test coverage percentage became 100%.

--- branchCoverage/my-app/src/test/java/TestCompute.java
+++ branchCoverage/my-app/src/test/java/TestCompute.java
@@ -9,9 +9,32 @@ public class TestCompute {
   Compute c;
 
   @Test
-  public void example() {
+  public void testCountNumberOfOccurrences_EmptyQueue() {
     MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(0);
     c = new Compute(mq);
-    assertTrue(true);
+    assertEquals(-1, c.countNumberOfOccurrences("test"));
+  }
+
+  // Assuming there are 3 elements in the queue for below tests.
+  @Test
+  public void testCountNumberOfOccurrences_ElementNotInQueue() {
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(3); 
+    when(mq.contains("test")).thenReturn(false);
+    c = new Compute(mq);
+    assertEquals(0, c.countNumberOfOccurrences("test"));
+  }
+
+  @Test
+  public void testCountNumberOfOccurrences_ElementInQueue() {
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(3); 
+    when(mq.contains("test")).thenReturn(true);
+    when(mq.getAt(0)).thenReturn("test");
+    when(mq.getAt(1)).thenReturn("abc");
+    when(mq.getAt(2)).thenReturn("test");
+    c = new Compute(mq);
+    assertEquals(2, c.countNumberOfOccurrences("test"));
   }
 }
--- branchCoverage/my-app/src/test/java/TestUtil.java
+++ branchCoverage/my-app/src/test/java/TestUtil.java
@@ -1,16 +1,40 @@
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestUtil {
-  Util c;
+    private Util util;
 
-  @Before
-  public void setUp() { c = new Util(); }
+    @Before
+    public void setUp() {
+        util = new Util();
+    }
 
-  @Test
-  public void example() { assertTrue(true); }
+    @Test
+    public void testComputeSingleArg() {
+        assertFalse(util.compute(5));
+    }
+
+    @Test
+    public void testComputeEvenArgs() {
+        assertFalse(util.compute(1, 2, 3, 4));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testComputeZeroArg() {
+        util.compute(0, 1, 2);
+    }
+
+    @Test
+    public void testComputeSumDivisibleByArg() {
+        assertTrue(util.compute(2, 3, 4));
+    }
+
+    @Test
+    public void testComputeSumNotDivisibleByArg() {
+        assertFalse(util.compute(2, 3, 8));
+    }
+
+  
 }
